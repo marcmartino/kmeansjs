0d6a78ce6992851226d53302f41aea6ad70c5e38
Marc <mrmarcmartino@gmail.com>
2016-03-10 21:30:05 -0800

    knn working, though it just takes one of the values if there is a tie for k

--- knn.js
+++ knn.js
@@ -1,51 +1,61 @@
 var xys = [{"x":[1,6],"y":7},{"x":[2,4],"y":8},{"x":[3,7],"y":16},{"x":[6,8],"y":44},{"x":[7,1],"y":50},{"x":[8,4],"y":68}];
 var euDist = (pt1, pt2) => Math.sqrt(Math.pow(pt1[0] - pt2[0], 2) + Math.pow(pt1[1] - pt2[1], 2));
-var manDist = (pt1, pt) => pt1[0] - pt2[0] + pt1[1] - pt2[1];
+var manDist = (pt1, pt2) => pt1[0] - pt2[0] + pt1[1] - pt2[1];
 var genArr = function (len) {
     return Array.apply(null, Array(len)).map(function () {
     });
 };
+var pluck = (prop) => (el) => el[prop];
 
-function kNN(data, k, distFunc) {
+function kNN(data) {
     "use strict";
-    var usingDistFunc = distFunc || euDist;
-    //nearestElements(data, k, distFunc);
-    var closestElems = data.reduce(nearestElements(k, distFunc), [])
-}
+    return function (pt, k, distFunc) {
+        var usingDistFunc = distFunc || euDist;
 
-function nearestElements(k, distFunc) {
-    "use strict";
-    return function (minPoints, dataPt, index) {
-        var minIndex;
-        if (minPoints.length < k) {
-            minPoints.push(dataPt);
-            return minPoints;
-        }
-        minIndex = Array.keys(minPoints).reduce(((dataPt) => (minI, thisMinPt, allPts) => {
-            var thisDist = distFunc(thisMinPt.x, dataPt.x),
-                minDist = distFunc(thisMinPt.x, allPts[minI].x);
-        })(dataPt), -1);
-
+        return data.reduce(nearestElements(pt,k, usingDistFunc), [])
+            .map(pluck('y')).reduce((total, val) => total + (val/k), 0);
     }
 }
 
-
-
-function findMinOfMins(allMins, thisMin) {
+function nearestElements(pt, k, distFunc) {
     "use strict";
-    return function (minIndex, thisIndex) {
-        debugger;
-        var existingDist = thisMin - (allMins[minIndex] || 0),
-            thisDist = thisMin - allMins[thisIndex];
-        if (thisDist < existingDist) {
-            return thisIndex;
+    return function (currentMins, potentialMin) {
+        var replaceIndex;
+
+        if (currentMins.length < k) {
+            currentMins.push(potentialMin);
+            return currentMins;
         }
-        return minIndex;
+        replaceIndex = currentMins.reduce(findMinIndex(pt, potentialMin, distFunc), -1);
+        if (replaceIndex >= 0) {
+            currentMins[replaceIndex] = potentialMin;
+        }
+        return currentMins;
     }
 }
-var tempMins = [2,3,1];
-var newMinIndex = [0,1,2].reduce(findMinOfMins(tempMins, 1.5), -1);
-if (newMinIndex >= 0) {
-    tempMins[newMinIndex] = 1.5;
+
+function findMinIndex(pt, testMinPt, distFunc) {
+    "use strict";
+    var distFromTestMin = distFunc(pt, testMinPt.x);
+    return function (runningMinIndex, estabilishedMinObj, establishedMinIndex, allEstablishedMinObj) {
+
+        var distFromEstablishedMin = distFunc(pt, estabilishedMinObj.x);
+
+        if (distFromTestMin < distFromEstablishedMin) {
+            if (runningMinIndex == -1) {
+                return establishedMinIndex;
+            }
+            else if (distFromTestMin < distFunc(pt, allEstablishedMinObj[runningMinIndex].x)) {
+                return establishedMinIndex;
+            }
+        }
+        return runningMinIndex;
+    }
 }
-console.log(newMinIndex, tempMins);
+
+var knnFunc = kNN(xys);
+
+console.log(knnFunc([4,2], 1));
+console.log(knnFunc([4,2], 1, manDist));
+console.log(knnFunc([4,2], 3));
+console.log(knnFunc([4,2], 3, manDist));
